fde60ab827aba96a878e4ce50afacf27bcf7b970
Janus <janus-olsen91@hotmail.com>
2020-05-03 11:26:22 +0200

    HP-1 Værelsesudlejning segregated

--- src/main/java/view/GuiSingleton.java
+++ src/main/java/view/GuiSingleton.java
@@ -1,9 +1,11 @@
 package view;
 
 import controller.ExcelConnection;
+import model.Værelsesudlejning;
 import view.main.BeboerlisteMenu;
 import view.main.GUI;
 import view.main.Hovedmenu;
+import view.main.VærelsesUdlejningsMenu;
 
 public class GuiSingleton {
 
@@ -13,6 +15,7 @@ public class GuiSingleton {
     public static Hovedmenu hovedmenu = new Hovedmenu();
     public static GUI gui= new GUI();
     public static ExcelConnection ec;
+    public static VærelsesUdlejningsMenu værelsesudlejningsmenu = new VærelsesUdlejningsMenu();
 
     private GuiSingleton(){}
 
--- src/main/java/view/main/GUI.java
+++ src/main/java/view/main/GUI.java
@@ -27,7 +27,6 @@ import java.util.ArrayList;
 
 public class GUI {
 	private Scene scene;
-	//static ExcelConnection ec;
 	static GUI_PopUps popUp = new GUI_PopUps();
 	GUI_PopUps_Deadlines popUpDead = new GUI_PopUps_Deadlines();
 	static GuiSingleton gui;
@@ -70,7 +69,7 @@ public class GUI {
 		Button fremlejeButton = new Button("Fremleje");
 		fremlejeButton.setOnAction(e -> fremlejeMenu(primaryStage));
 		Button værelsesudlejningsButton = new Button("Værelsesudlejning");
-		værelsesudlejningsButton.setOnAction(e -> værelsesUdlejningMenu(primaryStage));
+		værelsesudlejningsButton.setOnAction(e -> gui.værelsesudlejningsmenu.værelsesUdlejningMenu(primaryStage));
 
 		Label l = new Label("Ingen nuværende deadlines");
 		tView.setPlaceholder(l);
@@ -196,152 +195,6 @@ public class GUI {
 		return deadlines;
 	}
 
-	/**
-	 *
-	 * @param columnProps
-	 * @return
-	 */
-
-	/**
-	 * 
-	 * @param primaryStage
-	 *            : main stage
-	 */
-	@SuppressWarnings("unchecked")
-	private void værelsesUdlejningMenu(Stage primaryStage) {
-		BorderPane borderP = new BorderPane();
-		// venstre side
-		Button tilbageButton = new Button("Tilbage");
-		tilbageButton.setOnAction(e -> {
-			try {
-				hovedMenu(primaryStage);
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}
-		});
-
-		VBox vb = new VBox(tilbageButton);
-		vb.setPadding(new Insets(0,10,0,0));
-		borderP.setLeft(vb);
-
-		// Midten
-		TabPane tP = new TabPane();
-		borderP.setCenter(tP);
-
-		Tab tab1 = new Tab("Ledige værelser");
-		tab1.setClosable(false);
-		Tab tab2 = new Tab("Udlejede værelser");
-		tab2.setClosable(false);
-
-		tP.getTabs().addAll(tab1, tab2);
-
-		TableView<Værelsesudlejning> tView1 = new TableView<Værelsesudlejning>();
-		Button buttonStart = new Button("Kom i gang");
-		buttonStart.setOnAction(event -> {
-			popUp.opretLedigtVærelse(gui.ec, tView1);
-		});
-
-		tView1.setPlaceholder(buttonStart);
-		TableView<Værelsesudlejning> tView2 = new TableView<Værelsesudlejning>();
-
-		// Herunder oprettes en metode til at håndtere dobbeltklik
-		tView1.setRowFactory(tv -> {
-			TableRow<Værelsesudlejning> row = new TableRow<>();
-			row.setOnMouseClicked(event -> {
-				if (row.isEmpty() && event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2) {
-
-					popUp.opretLedigtVærelse(gui.ec, tView1);
-					tView1.refresh();
-				}
-				if (!row.isEmpty() && event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2) {
-
-					Værelsesudlejning clickedRow = row.getItem();
-					popUp.udfyldLedigtVærelse(gui.ec, tView1, tView2, clickedRow, false);
-					tView1.refresh();
-				}
-			});
-			return row;
-		});
-
-		TableColumn<Værelsesudlejning, String> værelseColumn = new TableColumn<Værelsesudlejning, String>("Værelse");
-		værelseColumn.setCellValueFactory(new PropertyValueFactory<>("værelse"));
-
-		TableColumn<Værelsesudlejning, LocalDate> indflytningColumn = new TableColumn<Værelsesudlejning, LocalDate>(
-				"Overtagelsesdato");
-		indflytningColumn.setCellValueFactory(new PropertyValueFactory<>("indflytningsdato"));
-
-		tView1.setItems(getVærelsesUdlejning(true));
-
-		TableColumn<Værelsesudlejning, String> værelseColumn2 = new TableColumn<Værelsesudlejning, String>("Værelse");
-		værelseColumn2.setCellValueFactory(new PropertyValueFactory<>("værelse"));
-
-		TableColumn<Værelsesudlejning, String> navnColumn2 = new TableColumn<Værelsesudlejning, String>("Navn");
-		navnColumn2.setCellValueFactory(new PropertyValueFactory<>("navn"));
-
-		TableColumn<Værelsesudlejning, LocalDate> indflytningColumn2 = new TableColumn<Værelsesudlejning, LocalDate>(
-				"Overtagelsesdato");
-		indflytningColumn2.setCellValueFactory(new PropertyValueFactory<>("indflytningsdato"));
-		TableColumn<Værelsesudlejning, String> behandletDatoColumn2 = new TableColumn<Værelsesudlejning, String>(
-				"Behandlingsdato");
-		behandletDatoColumn2.setCellValueFactory(new PropertyValueFactory<>("behandlingsdato"));
-		TableColumn<Værelsesudlejning, String> behandlerInitialerColumn2 = new TableColumn<Værelsesudlejning, String>(
-				"Behandler Initialer");
-		behandlerInitialerColumn2.setCellValueFactory(new PropertyValueFactory<>("behandlerInitialer"));
-		tView2.setItems(getVærelsesUdlejning(false));
-		tView2.setRowFactory(tv -> {
-			TableRow<Værelsesudlejning> row = new TableRow<>();
-			row.setOnMouseClicked(event -> {
-				if (!row.isEmpty() && event.getButton() == MouseButton.PRIMARY && event.getClickCount() == 2) {
-					// TODO Lav noget her der retter info ved værelsesudlejning
-					Værelsesudlejning clickedRow = row.getItem();
-					popUp.udfyldLedigtVærelse(gui.ec, tView1, tView2, clickedRow, true);
-					tView2.refresh();
-				}
-
-			});
-			return row;
-		});
-
-		tView1.getColumns().addAll(indflytningColumn, værelseColumn);
-		tView2.getColumns().addAll(indflytningColumn2, værelseColumn2, navnColumn2, behandletDatoColumn2,
-				behandlerInitialerColumn2);
-
-		tab1.setContent(tView1);
-		tab2.setContent(tView2);
-
-		scene = new Scene(borderP, 900, 700);
-		scene.getStylesheets().add("hekostyling.css");
-		primaryStage.setScene(scene);
-	}
-
-
-	/**
-	 * 
-	 * @param ledigeVærelser:
-	 *            True hvis værelset er ledigt
-	 * @return ObservableList med enten udlejede værelser eller ikke lejede værelser
-	 */
-	private ObservableList<Værelsesudlejning> getVærelsesUdlejning(boolean ledigeVærelser) {
-		ArrayList<Værelsesudlejning> alleVærelser = gui.ec.getVærelsesudlejning();
-		ArrayList<Værelsesudlejning> temp = new ArrayList<Værelsesudlejning>();
-		if (ledigeVærelser == true) {
-			for (Værelsesudlejning v : alleVærelser) {
-				if (v.getNavn().length() == 0) {
-					temp.add(v);
-				}
-			}
-		} else {
-			for (Værelsesudlejning v : alleVærelser) {
-				if (v.getNavn().length() > 0) {
-					temp.add(v);
-
-				}
-			}
-
-		}
-		ObservableList<Værelsesudlejning> værelser = FXCollections.observableArrayList(temp);
-		return værelser;
-	}
 
 	private void dispensationsMenu(Stage primaryStage, TableView<Deadline> tViewHMenu) {
 
